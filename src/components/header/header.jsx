9052c3c7337c1cad87fdde772ac1fa6b87a3f264
jmosqueralpr <jmosqueralpr@gmail.com>
2025-12-30 14:45:17 -0300

    Agregado boton de volver

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,18 +4,63 @@ import Prueba from "./prueba";
 import UserInfo from "./userInfo";
 import AuthContext from '../../context/authContext'; /* Para usar el contexto creado */
 import { UserCard } from "./userCard";
+import { useLocation, useNavigate } from "react-router-dom";
 import "../../styles/header.css";
 import "../../styles/userCard.css";
 
+
 const Header = ({ userName }) => {
-    const { authUser, setAuthUser } = useContext(AuthContext); /* Llamo a las variables del contexto. */
+  const { authUser } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  console.log("Estoy en el header:", location.pathname);
+
+  const hideBackButton =
+    location.pathname === "/" || location.pathname === "/Dashboard";
+
+  const handleBack = () => {
+    if (window.history.length > 1) {
+      navigate(-1);
+    } else {
+      navigate("/dashboard");
+    }
+  };
+
+  return (
+    <header className="header">
+        <div className="header-left">
+            <Logo />
+
+            {!hideBackButton && (
+            <button
+                className="back-button"
+                onClick={handleBack}
+                aria-label="Volver atrás"
+                title="Volver"
+                >
+                Volver
+                </button>
+
+            )}
+        </div>
+
+        <UserCard isFollowing={true} userName={authUser} name={authUser} />
+        </header>
+
+  );
+};
+
+
+/* const Header = ({ userName }) => {
+    const { authUser, setAuthUser } = useContext(AuthContext); 
     return (
         <header className="header">
             <Logo></Logo>
-            {/* <Prueba></Prueba> */}
+             <Prueba></Prueba> 
             <UserCard isFollowing={true} userName= {authUser} name={authUser}/>
         </header>
     )
 }
+    */
 
-export default Header;
+export default Header; 
